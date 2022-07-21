25f9f4a4719113495a8768bb9bdfa12563f9a6f9
Madhav Biju <madhavbiju01@gmail.com>
2022-07-21 22:05:11 +0530

    Streamlit: Move Movie Titles to the Bottom

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 
-
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
@@ -59,25 +58,24 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
+    st.markdown("<h5 style='color: white;'> </h4>", unsafe_allow_html=True)
     st.write("Recommended Movies based on your interests are :")
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
+        st.text(recommended_movie_names[0])
     with col2:
-        st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
+        st.text(recommended_movie_names[1])
     with col3:
-        st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
+        st.text(recommended_movie_names[2])
     with col4:
-        st.text(recommended_movie_names[3])
         st.image(recommended_movie_posters[3])
+        st.text(recommended_movie_names[3])
     with col5:
-        st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
+        st.text(recommended_movie_names[4])  
 
 st.title(" ")
-# st.write("The code for this recommender system is available [here](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
